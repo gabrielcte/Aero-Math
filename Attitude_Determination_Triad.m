aa3908b1cff646aa85a0d9cee449e1943e90e98a
Gabriel <40703284+gabrielcte@users.noreply.github.com>
2021-08-30 23:50:06 -0300

    Create Attitude_Determination_Triad.m

---
+++ Attitude_Determination_Triad.m
@@ -0,0 +1,24 @@
+%{
+Gabriel Alves Silva
+TRIAD é um algoritmo de determinação de atitude simples e rápida.
+Essa usa dois vetores em dois sistemas de coordenadas diferentes.
+Os vetores devem ser diferentes.
+
+ver:
+
+%}
+
+function [Rbi] = Attitude_Determination_Triad(VB, VI) 
+
+    t1b = VB(1,:)';
+    t1i = VI(1,:)';  
+    t2b = cross(VB(1,:)',VB(2,:)')/norm(cross(VB(1,:)',VB(2,:)'));
+    t2i = cross(VI(1,:)',VI(2,:)')/norm(cross(VI(1,:)',VI(2,:)'));  
+    t3b = cross(t1b,t2b)/norm(cross(t1b,t2b));
+    t3i = cross(t1i,t2i)/norm(cross(t1i,t2i));    
+    Ab = [t1b t2b t3b];
+    Ai = [t1i t2i t3i];
+    Rbi= Ab*Ai';
+
+    
+end
